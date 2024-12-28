b6018621ec8af5ad326a63fc5dcb33727a898b1b
Musa <inf4096@hs-worms.de>
2024-12-28 21:20:38 +0100

    Fehlender dotenv installiert für Projekt und alle routen nochmals geteetet

--- backend/src/route/route.ts
+++ backend/src/route/route.ts
@@ -9,15 +9,15 @@ const router = express.Router();
 
 // Nutzer-Routen
 router.post('/user/createUser', createUser);
-router.delete('/user/deleteUser', deleteUser);
-router.get('/user/getUserData', getUserData);
+router.delete('/user/deleteUser', deleteUser);  // als Params muss aber als body
+router.get('/user/getUserData', getUserData);   // als Params muss aber als body
 
 // Feedback-Routen
 router.post('/feedback/createFeedback', createFeedback);
 
 // EventCosts-Routen
 router.post('/eventCosts/create', createEventCost);
-router.get('/eventCosts', getEventCost);
+router.get('/eventCosts', getEventCost); // als Params 
 router.put('/eventCosts/update', updateEventCost);
 router.delete('/eventCosts/delete', deleteEventCost);
 
@@ -30,8 +30,8 @@ router.delete('/transaction/delete', deleteTransaction);
 // Bookings-Routen
 router.post('/booking/create', createBooking);
 router.post('/booking/get', getBooking); // um eine spezifische Buchung abzurufen
-router.get('/bookings', getAllBookings);
-router.put('/booking/update', updateBooking);
+//router.get('/bookings', getAllBookings);
+//router.put('/booking/update', updateBooking);
 router.delete('/booking/delete', deleteBooking);
 
 export { router };
