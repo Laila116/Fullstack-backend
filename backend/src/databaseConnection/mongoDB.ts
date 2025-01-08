e17512102c89e0f9bccfa962d847f4b79d9f26b6
Musa <inf4096@hs-worms.de>
2025-01-09 00:29:24 +0100

    .env datei eingerichtet

--- backend/src/databaseConnection/mongoDB.ts
+++ backend/src/databaseConnection/mongoDB.ts
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 //const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/mongo_eventdb";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mongo_eventdb";
 
 async function connectToMongoDB(): Promise<boolean> {
   try {
--- backend/src/databaseConnection/postgres.ts
+++ backend/src/databaseConnection/postgres.ts
@@ -1,10 +1,15 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 
-const host = process.env.DATABASE_HOST || 'localhost';
-
-const sequelize = new Sequelize('eventdb', 'AdminUser', 'AdminPassword', { //von Compose datei für verbindung
-  host: host,
-  port: 5432,
+const dbName = process.env.PG_NAME || 'postgre_eventdb';
+const dbUser = process.env.PG_USER || 'AdminUser';
+const dbPassword = process.env.PG_PASSWORD || 'AdminPassword';
+const dbHost = process.env.DATABASE_HOST || 'localhost';
+const dbPort = parseInt(process.env.PG_PORT || '5432', 10);
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, { //von Compose datei für verbindung
+  host: dbHost,
+  port: dbPort,
   dialect: 'postgres',
   dialectOptions: {
     connectTimeout: 60000 
